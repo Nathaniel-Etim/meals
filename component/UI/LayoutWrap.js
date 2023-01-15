a6e6f13b52267fcb5cfd3c1e2a090018697559cc
Nathaniel <nathjoeetim@gmail.com>
2023-01-15 20:44:42 +0100

    wrapper module

--- component/UI/LayoutWrap.js
+++ component/UI/LayoutWrap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./Layout";
-import classes from "./layoutwrapper.module.css";
+import classes from "./layoutWrapper.module.css";
 
 const LayoutWrap = (props) => {
   return (
--- pages/[mealId]/index.js
+++ pages/[mealId]/index.js
@@ -50,7 +50,7 @@ export async function getStaticPaths() {
 
   client.close();
   return {
-    fallback: true,
+    fallback: "blocking",
     paths: meal.map((meal) => {
       return {
         params: {
